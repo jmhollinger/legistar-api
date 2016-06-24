f5a5d943888262019ac8769cfad764679b3d71f2
Jonathan Hollinger <jmholl5@gmail.com>
2016-06-24 17:32:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,10 +21,9 @@ var https_redirect = function(req, res, next) {
 
 app.use(https_redirect);
 
-app.use(function(req, res, next) {
+app.use(function(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
 });
 
 app.use(express.static('public'));
